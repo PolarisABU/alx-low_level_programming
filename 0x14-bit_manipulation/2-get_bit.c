fcfb0659d5cf96cbfac3884edce68b395bd5471a
PolarisABU <oluwadunsinolonitola@gmail.com>
2022-08-17 01:33:22 -0700

    Bit Manipulations

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#inclue "main.h"
+
+/**
+ * get_bit - value of abit at a given index
+ * @n: decimal parameter
+ * @index: index
+ * Return: val
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+
+	bit = (n >> index);
+	if (index . 32)
+		return (-1);
+	return (bit & 1);
+}
